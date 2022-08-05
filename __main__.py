c4e3edcb887c8f684fb088d2c66a6df4f7c49d4d
akash-forte <aviswan@forte.io>
2022-08-05 15:37:26 -0600

    creating gke cluster

--- __main__.py
+++ __main__.py
@@ -1,10 +1,36 @@
-"""A Google Cloud Python Pulumi program"""
+#create GKE Cluster and nodepools
 
 import pulumi
-from pulumi_gcp import storage
+import pulumi_gcp as gcp
 
-# Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket', location="US")
+gke_sa = gcp.serviceaccount.Account("gke_sa",
+    account_id="gke-sa",
+    display_name="GKE Service Account")
+primary = gcp.container.Cluster("primary",
+    location="us-central1",
+    remove_default_node_pool=True,
+    initial_node_count=1)
+primary_spot_nodes = gcp.container.NodePool("primary-spot-nodes",
+    location="us-central1",
+    cluster=primary.name,
+    autoscaling = gcp.container.NodePoolAutoscalingArgs(
+        max_node_count = 1,
+        min_node_count = 0
+    ),
+    node_count=0,
+    node_config=gcp.container.NodePoolNodeConfigArgs(
+        spot=True,
+        machine_type="e2-micro",
+        service_account=gke_sa.email,
+        oauth_scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    ))
 
-# Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
+ondemand_nodes = gcp.container.NodePool("on-demand-nodes",
+    location="us-central1",
+    cluster=primary.name,
+    node_count=0,
+    node_config=gcp.container.NodePoolNodeConfigArgs(
+        machine_type="e2-micro",
+        service_account=gke_sa.email,
+        oauth_scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    ))
